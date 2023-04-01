0641fbce1598dbf13470ebdd60d7359be7fa6f34
samchillcott1 <samchillcott@gmail.com>
2023-04-01 11:30:58 +0100

    add structure to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+// import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello react</h1>
+      <header>
+        <h1>Sam's Todo List</h1>
+        <Form />
+        <TodoList />
+      </header>
     </div>
   );
 }
