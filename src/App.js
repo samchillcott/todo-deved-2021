51a6b07e9577b16d8fbe23a5e33f86ba11534402
samchillcott1 <samchillcott@gmail.com>
2023-04-02 11:53:01 +0100

    add complete handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import { useState } from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,14 +2,29 @@ import React from 'react'
 
 const Todo = ({ text, id, todos, setTodos, todo }) => {
   const deleteHandler = () => {
-    // console.log(e);
     setTodos(todos.filter((el) => el.id !== todo.id))
+  }
+
+  const completeHandler = () => {
+    setTodos(todos.map((item => {
+      if (item.id === todo.id) {
+        return {
+          ...item, complete: !item.complete
+        }
+      }
+      return item
+    })))
   }
 
   return (
     <div className='todo'>
-      <li className='todo-item'>{ text }</li>
-      <button className='complete-button'>
+      <li className={ `todo-item ${todo.completed ? "complete" : ""}` }>
+        { text }
+      </li>
+      <button
+        className='complete-button'
+        onClick={ completeHandler }
+      >
         <i className='fas fa-check'></i>
       </button>
       <button
